a0b0cb5640fbddfddf8bf22739d4e17667b81763
anamulhaque22 <anamulah535367@gmail.com>
2022-06-17 12:07:36 +0600

    order placing

--- module-66-1/genius-car-service-server/index.js
+++ module-66-1/genius-car-service-server/index.js
@@ -25,7 +25,7 @@ async function run() {
         });
 
 
-        //single service
+        //get single service
         app.get('/service/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
